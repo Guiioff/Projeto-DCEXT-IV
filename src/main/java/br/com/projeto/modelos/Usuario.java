6016409ed1b436f54a5bb3333739ec684461bf93
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-14 20:39:59 -0300

    feat(entidade): configuração dos atributos com as anotações do JPA

--- src/main/java/br/com/projeto/modelos/Usuario.java
+++ src/main/java/br/com/projeto/modelos/Usuario.java
@@ -1,12 +1,29 @@
 package br.com.projeto.modelos;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
 public class Usuario {
-    private Long id;
-    private String nome;
-    private String email;
-    private String senha;
-    private Date dataCadastro;
-    private Date dataNascimento;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id_usuario")
+  private Long id;
+
+  @Column(nullable = false, length = 150)
+  private String nome;
+
+  @Column(nullable = false, length = 100, unique = true)
+  private String email;
+
+  @Column(nullable = false, length = 100)
+  private String senha;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date dataCadastro;
+
+  @Column(nullable = false)
+  private Date dataNascimento;
 }
