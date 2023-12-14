02d4214fa6aaf1520019f539b6b1a23af90c499e
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-14 20:42:42 -0300

    feat(entidade): adição das anotações do Lombock à classe Usuário

--- src/main/java/br/com/projeto/modelos/Usuario.java
+++ src/main/java/br/com/projeto/modelos/Usuario.java
@@ -1,10 +1,18 @@
 package br.com.projeto.modelos;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
   @Id
