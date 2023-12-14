53eca76ef652bad5e3779940317008469c9c7b6d
Guiioff <guilhermerivaldy288@hotmail.com>
2023-12-14 17:47:37 -0300

    first commit

---
+++ src/main/java/br/com/projeto/ProjetoDcext4Application.java
@@ -0,0 +1,13 @@
+package br.com.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDcext4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDcext4Application.class, args);
+	}
+
+}
