cd2c492ca39cd23ed071aa3fff90882adb0556dd
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-14 20:37:14 -0300

    feat(entidade): criação dos atributos da entidade Usuário

--- src/main/java/br/com/projeto/modelos/Usuario.java
+++ src/main/java/br/com/projeto/modelos/Usuario.java
@@ -1,3 +1,12 @@
 package br.com.projeto.modelos;
 
-public class Usuario {}
+import java.util.Date;
+
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private Date dataCadastro;
+    private Date dataNascimento;
+}
