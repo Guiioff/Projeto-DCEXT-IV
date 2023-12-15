664e1ae2d372ac237d1785bec7ff1e537a4ddfed
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-15 11:02:50 -0300

    feat(usuário): declaração do método de busca por email no repositório

--- src/main/java/br/com/projeto/repositorios/UsuarioRepositorio.java
+++ src/main/java/br/com/projeto/repositorios/UsuarioRepositorio.java
@@ -4,5 +4,9 @@ import br.com.projeto.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {}
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+  Optional<Usuario> findByEmail(String email);
+}
