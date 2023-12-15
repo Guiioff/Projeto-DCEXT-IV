46e23eb36c79868d9ab263a5cc759c777d205dc8
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-15 10:51:52 -0300

    feat(usuário): configuração da camada repositório do usuário

--- src/main/java/br/com/projeto/repositorios/UsuarioRepositorio.java
+++ src/main/java/br/com/projeto/repositorios/UsuarioRepositorio.java
@@ -1,3 +1,8 @@
 package br.com.projeto.repositorios;
 
-public interface UsuarioRepositorio {}
+import br.com.projeto.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {}
