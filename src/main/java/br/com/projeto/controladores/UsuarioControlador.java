8e58fd82b6e8c92cc85c778d2069340fc1c57a64
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-15 11:15:36 -0300

    feat(usuário): configuração do controlador do usuário

--- src/main/java/br/com/projeto/controladores/UsuarioControlador.java
+++ src/main/java/br/com/projeto/controladores/UsuarioControlador.java
@@ -1,3 +1,11 @@
 package br.com.projeto.controladores;
 
-public class UsuarioControlador {}
+import br.com.projeto.servicos.UsuarioServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsuarioControlador {
+
+  @Autowired private UsuarioServico usuarioServico;
+}
