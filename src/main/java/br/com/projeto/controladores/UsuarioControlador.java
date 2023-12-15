e55d36714553513af01a5b20761d6584380c99cb
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-15 11:18:19 -0300

    feat(usuário): finalização do controlador do usuário e criação do método de cadastro de usuários

--- src/main/java/br/com/projeto/controladores/UsuarioControlador.java
+++ src/main/java/br/com/projeto/controladores/UsuarioControlador.java
@@ -1,11 +1,24 @@
 package br.com.projeto.controladores;
 
+import br.com.projeto.dtos.UsuarioDTO;
 import br.com.projeto.servicos.UsuarioServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/usuario")
 public class UsuarioControlador {
 
   @Autowired private UsuarioServico usuarioServico;
+
+  @PostMapping("/cadastro")
+  public ResponseEntity<String> cadastrarUsuário(@RequestBody UsuarioDTO usuarioDTO) {
+    this.usuarioServico.cadastrarUsuario(usuarioDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body("Usuário cadastrado com sucesso");
+  }
 }
