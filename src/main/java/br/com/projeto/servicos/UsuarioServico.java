39ae138cdf2b9ca15c96d69297ca9f1588d13847
Gabriel Silva <gabrielfsilva936@gmail.com>
2023-12-15 10:54:01 -0300

    feat(usuário): configuração da classe de serviço do usuário

--- src/main/java/br/com/projeto/servicos/UsuarioServico.java
+++ src/main/java/br/com/projeto/servicos/UsuarioServico.java
@@ -1,3 +1,11 @@
 package br.com.projeto.servicos;
 
-public class UsuarioServico {}
+import br.com.projeto.repositorios.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServico {
+
+  @Autowired private UsuarioRepositorio usuarioRepositorio;
+}
