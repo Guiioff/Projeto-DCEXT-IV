8677a7dd387a0253776c0f2158663d6a404acb61
Gabriel Silva <gabrielfsilva936@gmail.com>
2024-01-10 11:04:38 -0300

    feat(JwtService): implementação dos métodos para geração da chave de assinatura e para extração das claims

--- src/main/java/br/com/projeto/servicos/JwtService.java
+++ src/main/java/br/com/projeto/servicos/JwtService.java
@@ -1,8 +1,15 @@
 package br.com.projeto.servicos;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +21,21 @@ public class JwtService {
   @Value("${jwt.tempo.expiracao}")
   private Long jwtTempoExpiracao;
 
+  private <T> T extrairClaim(String token, Function<Claims, T> funcaoClaim) {
+    Claims claims = extrairTodasClaims(token);
+    return funcaoClaim.apply(claims);
+  }
 
+  private Claims extrairTodasClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getChaveAssinatura())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  private Key getChaveAssinatura() {
+    byte[] chaveBytes = Decoders.BASE64.decode(this.jwtSecretKey);
+    return Keys.hmacShaKeyFor(chaveBytes);
+  }
 }
