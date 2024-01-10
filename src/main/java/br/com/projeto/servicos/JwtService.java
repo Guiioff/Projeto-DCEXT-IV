0b61db38498f02ab0369122777d08a8ab267db93
Gabriel Silva <gabrielfsilva936@gmail.com>
2024-01-10 11:09:26 -0300

    feat(JwtService): implementação dos métodos para geração e verificação da expiração do token

--- src/main/java/br/com/projeto/servicos/JwtService.java
+++ src/main/java/br/com/projeto/servicos/JwtService.java
@@ -2,13 +2,17 @@ package br.com.projeto.servicos;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -20,6 +24,24 @@ public class JwtService {
 
   @Value("${jwt.tempo.expiracao}")
   private Long jwtTempoExpiracao;
+
+  private String doGerarToken(Map<String, Object> claims, UserDetails usuario) {
+    return Jwts.builder()
+        .setClaims(claims)
+        .setSubject(usuario.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + this.jwtTempoExpiracao))
+        .signWith(getChaveAssinatura(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  private boolean isTokenExpirado(String token) {
+    return extrairExpiracao(token).before(new Date(System.currentTimeMillis()));
+  }
+
+  private Date extrairExpiracao(String token) {
+    return extrairClaim(token, Claims::getExpiration);
+  }
 
   private <T> T extrairClaim(String token, Function<Claims, T> funcaoClaim) {
     Claims claims = extrairTodasClaims(token);
